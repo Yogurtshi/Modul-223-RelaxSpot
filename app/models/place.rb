4396bfc8bb4b0a69caa44f0c9c647782246f5fa3
yogurtshi <yoshi.gyger@bbzbl-it.ch>
2026-09-21 14:57:19 +0200

    model place added and tested

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,31 @@
 class Place < ApplicationRecord
+  enum :category, {
+    seating: 0,
+    smoking: 1,
+    shade: 2,
+    toilet: 3
+  }, default: :seating
+
+  enum :status, {
+    open: 0,
+    occupied: 1,
+    closed: 2,
+    dirty: 3
+  }, default: :open
+
+  belongs_to :proposed_by,
+             class_name: "User",
+             inverse_of: :proposed_places
+
+  belongs_to :locked_by,
+             class_name: "User",
+             optional: true,
+             inverse_of: :locked_places
+
+  has_many :check_ins, dependent: :destroy
+  has_many :status_reports, dependent: :destroy
+
+  validates :name, presence: true
+  validates :latitude, :longitude, presence: true
+  validates :capacity, numericality: { only_integer: true, greater_than: 0 }
 end
