98d9ee242b47931b4e19023fd931741014f346d0
yogurtshi <yoshi.gyger@bbzbl-it.ch>
2026-09-21 15:44:58 +0200

    check_in flow done

--- test/controllers/check_ins_controller_test.rb
+++ test/controllers/check_ins_controller_test.rb
@@ -1,24 +1,50 @@
 require "test_helper"
 
 class CheckInsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_check_in_url
-    assert_response :success
+  setup do
+    @user = User.create!(
+      name: "Check-in User",
+      email: "check-in-user@example.com",
+      password: "secure-password"
+    )
+
+    post session_url, params: {
+      email: @user.email,
+      password: "secure-password"
+    }
   end
 
-  test "should create check-in request" do
-    post check_ins_url, params: {
-      check_in: {
-        place_id: places(:one).id,
-        expected_minutes: 30
-      }
+  test "should get new" do
+    get new_check_in_url, params: {
+      place_id: places(:one).id
     }
 
     assert_response :success
   end
 
-  test "should get show" do
-    get check_in_url(check_ins(:one))
+  test "should create check-in" do
+    assert_difference("CheckIn.count", 1) do
+      post check_ins_url, params: {
+        check_in: {
+          place_id: places(:one).id,
+          expected_minutes: 30
+        }
+      }
+    end
+
+    assert_redirected_to check_in_url(CheckIn.order(:created_at).last)
+  end
+
+  test "should get own check-in" do
+    check_in = CheckIn.create!(
+      place: places(:one),
+      user: @user,
+      started_at: Time.current,
+      ends_at: 30.minutes.from_now
+    )
+
+    get check_in_url(check_in)
+
     assert_response :success
   end
 end
